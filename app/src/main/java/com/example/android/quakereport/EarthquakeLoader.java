94970d4e7ad7d1a17e7e71fec5602e173ff9f1bf
Miguel Angel Velazco <velazcomtz.miguel@live.com>
2017-05-29 21:45:23 -0500

    New preference
    Now you can sort events by magnitude or time

--- app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -2,6 +2,9 @@ package com.example.android.quakereport;
 
 import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.util.List;
@@ -11,7 +14,7 @@ import java.util.List;
  */
 class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
 
-    private static final String USGS_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+    private static final String USGS_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query";
 
     private static final String LOG_TAG = EarthquakeLoader.class.getName();
 
@@ -28,7 +31,27 @@ class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
 
     @Override
     public List<Earthquake> loadInBackground() {
+        Context ctx = getContext();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+
+        String minMagnitude = sharedPrefs.getString(
+                ctx.getString(R.string.settings_min_magnitude_key),
+                ctx.getString(R.string.settings_min_magnitude_default));
+
+        String orderBy = sharedPrefs.getString(
+                ctx.getString(R.string.settings_order_by_key),
+                ctx.getString(R.string.settings_order_by_default)
+        );
+
+        Uri baseUri = Uri.parse(USGS_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
         Log.i(LOG_TAG, "loadInBackground executed!");
-        return QueryUtils.fetchEarthquakeData(USGS_URL);
+        return QueryUtils.fetchEarthquakeData(uriBuilder.toString());
     }
 }
